2c4d458b534aebdfd08fd5180bee0f5d7c05ead7
Josh Orndorff <jorndorff@pingry.org>
2017-11-13 11:13:09 -0500

    Updated to parse trial words from file rather than being hard-coded.

--- HashRunner.java
+++ HashRunner.java
@@ -1,18 +1,25 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class HashRunner {
     private static List<Hasher> students;
     private static List<String> words;
 
 
-    public static void main(String [] args){
+    public static void main(String [] args) throws FileNotFoundException {
         // Load the hashers
         students = loadHashers();
 
         // Read words in from file
-        //String filename
-        words = loadWords();
+        String filename = "100Words.txt";
+        if (args.length > 0) {
+          filename = args[0];
+        }
+
+        words = loadWords(filename);
 
         // Test each of the hashers
         for(Hasher h : students)
@@ -48,70 +55,15 @@ public class HashRunner {
        return hashers;
     }
 
-    private static List<String> loadWords()
-    {
+    private static List<String> loadWords(String filename) throws FileNotFoundException {
         List<String> words = new ArrayList<>();
-        words.add("dog");
-        words.add("god");
-        words.add("trophy");
-        words.add("tree");
-        words.add("bear");
-        words.add("leak");
-        words.add("fish");
-        words.add("caller");
-        words.add("lake");
-        words.add("turing");
-        words.add("cellar");
-        words.add("cat");
-        words.add("spongebob");
-        words.add("egoes");
-        words.add("psychology");
-        words.add("recall");
-        words.add("goes");
-        words.add("supercalifragilisticexpialidocious");
-        words.add("onomatopoeia");
-        words.add("together");
-        words.add("aches");
-        words.add("face");
-        words.add("eggplant");
-        words.add("thaw");
-        words.add("what");
-        words.add("faceplant");
-        words.add("cafe");
-        words.add("facepalm");
-        words.add("its");
-        words.add("sit");
-        words.add("language");
-        words.add("xylophone");
-        words.add("pterodactyl");
-        words.add("bologna");
-        words.add("orangatang");
-        words.add("shall");
-        words.add("halls");
-        words.add("orange");
-        words.add("cosmic");
-        words.add("comics");
-        words.add("kale");
-        words.add("vase");
-        words.add("save");
-        words.add("dew");
-        words.add("wed");
-        words.add("rebuild");
-        words.add("builder");
-        words.add("Pneumonoultramicroscopicsilicovolcanoconiosis");
-        words.add("icon");
-        words.add("coin");
-        //words.add("none");
-        words.add("neon");
-        words.add("cast");
-        words.add("acts");
-        words.add("cats");
-        words.add("relay");
-        words.add("layer");
-        words.add("early");
-        words.add("eats");
-        words.add("seat");
-        words.add("teas");
+        Scanner s = new Scanner(new File(filename));
+
+        while (s.hasNextLine()){
+            words.add(s.nextLine());
+        }
+
+        s.close();
         return words;
     }
 
