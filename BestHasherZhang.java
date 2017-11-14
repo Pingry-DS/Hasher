8c9325ac6484085a7404160a9bf6ca80c74ae4e3
Will Zhang <wzhang2018@pingry.org>
2017-11-14 13:25:57 -0500

    removed extraneous code

--- BestHasherZhang.java
+++ BestHasherZhang.java
@@ -4,13 +4,7 @@ public class BestHasherZhang implements Hasher {
 
 
   public int hash(String s){
-    int numOfConsonants = 0;
-    for(int i = 1; i <= s.length(); i++){
-      if( !s.substring(i-1, i).equals("a") || !s.substring(i-1, i).equals("e") ||
-          !s.substring(i-1, i).equals("i") || !s.substring(i-1, i).equals("o") || !s.substring(i-1, i).equals("u")){
-        numOfConsonants++;
-      }
-    }
+
     int firstLetter = (int) s.charAt(0);
     int sum = firstLetter ^ firstLetter;
     sum = sum >> firstLetter;
